ecc0ea37a58db3388d997081dda3a77da4852f7a
Touhedul Islam <touhedcs@gmail.com>
2025-06-04 22:10:29 +0600

    Add files via upload
    Uploaded

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sora: ['Sora', 'sans-serif'],
+      },
+    },
+  },
+  content: ["./**/*.html"], // adjust if needed
+  plugins: [],
+};
